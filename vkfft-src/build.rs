caac868a811fcdbb0f63c27f3b9443664eed8397
周睿 <zhourui818@126.com>
2022-12-08 14:59:10 +0800

    init

--- vkfft-src/build.rs
+++ vkfft-src/build.rs
@@ -96,107 +96,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("cargo:rustc-link-lib={}", library);
     }
 
-
-    //
-    //
-    // let mut cmake_cmd = Command::new("cmake");
-    //
-    // cmake_cmd.arg("-S").arg(PathBuf::from(&manifest_dir).join("wrapper"));
-    // cmake_cmd.arg("-DCMAKE_BUILD_TYPE=Release");
-    // cmake_cmd.arg(format!("-DCMAKE_INSTALL_PREFIX={}", out_dir.display()));
-    // cmake_cmd.arg("-Dbuild_VkFFT_FFTW_precision=off");
-    //
-    // if target_os == "android" {
-    //     let ndk_home = PathBuf::from(env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set!"));
-    //     let toolchain_file = ndk_home.join("build").join("cmake").join("android.toolchain.cmake");
-    //
-    //
-    //     cmake_cmd.args(["-G", "Ninja"]);
-    //     cmake_cmd.arg(format!("-DCMAKE_TOOLCHAIN_FILE={}", toolchain_file.display()));
-    //
-    //     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    //     let android_abi;
-    //     match  arch.as_str(){
-    //         "x86" => {
-    //             android_abi="x86"
-    //         }
-    //         "x86_64" => {
-    //             android_abi="x86_64"
-    //         }
-    //         "arm" => {
-    //             android_abi="armeabi-v7a"
-    //         }
-    //         "aarch64" => {
-    //             android_abi="arm64-v8a"
-    //         }
-    //         _ =>{
-    //             panic!("arch not support: {}", arch);
-    //         }
-    //     }
-    //
-    //     cmake_cmd.arg("-DANDROID_PLATFORM=android-26");
-    //     cmake_cmd.arg(format!("-DANDROID_ABI={}", android_abi));
-    //
-    //
-    // }
-    // let cmake_build_dir = out_dir.join("build");
-    // cmake_cmd.arg("-B").arg(cmake_build_dir.clone());
-    //
-    // println!("running: {:?}", cmake_cmd);
-    //
-    //
-    // let status = match cmake_cmd.status() {
-    //     Ok(status) => status,
-    //     Err(ref e) if e.kind() == ErrorKind::NotFound => {
-    //         panic!(
-    //             "failed to execute command: {}\nis `{}` not installed?",
-    //             e, "cmake"
-    //         );
-    //     }
-    //     Err(e) => panic!("failed to execute command: {}", e),
-    // };
-    // if !status.success() {
-    //     panic!(
-    //         "command did not execute successfully, got: {}",
-    //         status
-    //     );
-    // }
-    //
-    // cmake_cmd = Command::new("cmake");
-    //
-    // cmake_cmd.arg("--build").arg(cmake_build_dir.clone())
-    //     .arg("--config").arg("Release")
-    //     .arg("--target").arg("install")
-    //     .arg("-j").arg("12");
-    //
-    // println!("running: {:?}", cmake_cmd);
-    //
-    // let status = cmake_cmd.status()?;
-    //
-    // if !status.success() {
-    //
-    //     panic!("run cmd fail: {:?}", cmake_cmd)
-    // }
-    //
-    //
-    // let lib_dir = out_dir.join("lib").display().to_string();
-    // let include_dir = out_dir.join("include").display().to_string();
-    //
-    // println!("cargo:rustc-link-search=native={}", lib_dir.as_str());
-    //
-    // if target_os=="windows" {
-    //     libraries.push("vulkan-1");
-    // }else {
-    //     libraries.push("vulkan");
-    // }
-    //
-    //
-    // for library in libraries.iter() {
-    //     println!("cargo:rustc-link-lib={}", library);
-    // }
-
-
-
     let include_dirs = [
         format!("{}/vkFFT", vkfft_root.display()),
         format!("{}/include/glslang/Include", build_args.out_dir.display()),
@@ -208,10 +107,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     ];
 
 
-
-
-    let bindings = gen_wrapper(&rw, &defines, &include_dirs)?;
-    bindings.write_to_file(build_args.out_dir.join("bindings.rs"))?;
+    // let bindings = gen_wrapper(&rw, &defines, &include_dirs)?;
+    // bindings.write_to_file(build_args.out_dir.join("bindings.rs"))?;
 
     Ok(())
 }
@@ -301,8 +198,6 @@ fn gen_wrapper<F, const N: usize, const M: usize>(file: F,  defines: &[(&str, &s
     println!("{:?}", clang_args);
 
 
-
-
     let res = bindgen::Builder::default()
         .clang_args(clang_args)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -322,7 +217,6 @@ fn gen_wrapper<F, const N: usize, const M: usize>(file: F,  defines: &[(&str, &s
         .allowlist_function("initializeVkFFT")
         .allowlist_function("deleteVkFFT")
         .allowlist_function("VkFFTGetVersion")
-
         .generate();
 
     let bindings = match res {
