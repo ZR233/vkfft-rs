bfae2dbcca0b105d6226690cb188f2c2538eca6f
周睿 <zhourui818@126.com>
2022-12-08 12:15:28 +0800

    init

--- vkfft-src/src/lib.rs
+++ vkfft-src/src/lib.rs
@@ -2,19 +2,3 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
